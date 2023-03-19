a54d39003c40d3e60bfed305abcbe08a58128a62
VtlBz <VtlBz@users.noreply.github.com>
2023-03-20 00:17:27 +0500

    HW_s16_v.0.1.19

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView, RedirectView
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
